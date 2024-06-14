06cdeeb6672ca80d6840346986a3eed90b69791d
jerespi <jeremie.spiesser@gmail.com>
2024-06-14 09:48:18 +0200

    en passant la mut ref du stack en argument du constructeur

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ enum Opt {
     Number(f64),
     OneOpt(OneOpt),
     TwoOpt(TwoOpt),
+    UtilOpt(UtilOpt),
 }
 
 #[derive(Debug)]
@@ -25,8 +26,32 @@ enum OneOpt{
     Cubed,
     Sqrt,
 }
+
+#[derive(Debug)]
+enum UtilOpt{
+    Print,
+    PrintAll,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct ParseUtilOptErr;
+
+impl FromStr for UtilOpt{
+    type Err = ParseUtilOptErr; 
+    fn from_str(input: &str) -> Result<UtilOpt, Self::Err> {
+        match input.trim() {
+            "p" => Ok(UtilOpt::Print),
+            "pa" => Ok(UtilOpt::PrintAll),
+            _ => Err(ParseUtilOptErr),
+        }
+    }
+}
+
+
 #[derive(Debug, PartialEq, Eq)]
 struct ParseOneOptErr;
+
+
 impl FromStr for OneOpt{
     type Err = ParseOneOptErr; 
     fn from_str(input: &str) -> Result<OneOpt, Self::Err> {
@@ -53,6 +78,9 @@ impl FromStr for Opt {
         if let Ok(to) = TwoOpt::from_str(input.trim()) {
             return Ok(Opt::TwoOpt(to));
         }
+        if let Ok(uo) = UtilOpt::from_str(input.trim()) {
+            return Ok(Opt::UtilOpt(uo));
+        }
         Err(ParseOptErr)
     }
 }
@@ -74,68 +102,105 @@ impl FromStr for TwoOpt{
 
 
 struct RPNStack<'a> {
-    stack : &'a mut Box<Vec<f64>>
+    stack : &'a mut Vec<f64>
 }
 
 impl<'a> RPNStack<'a> {
 
-    fn new() {
+    fn new(stack : &'a mut Vec<f64> ) -> RPNStack<'a> {
         RPNStack{
-            stack: &mut Box::new(Vec::<f64>::new())
+            stack: stack
         }
     }
 
-    fn print(&self) {
-        dbg!(self.stack);
+    fn print_top(&self) {
+        if let Some(top) = self.stack.last() {
+            println!("{}", top);
+        }
     }
 
-    fn handleOpt(&mut self, opt : &Opt) {
+    fn print_all(&self) {
+        for number in self.stack.iter().rev() {
+            println!("{}", number);
+        }
+    }
+
+    fn handle_opt(&mut self, opt : &Opt) {
         match opt {
             Opt::Number(f) => self.stack.push(f.clone()),
             Opt::OneOpt(oo) => match oo {
                 OneOpt::Squared => {
                     if let Some(top) = self.stack.pop() {
                         self.stack.push(top * top);
+                        self.print_top();
                     }
                 },
-                _ => todo!("not implemented yet"),
+                OneOpt::Cubed => {
+                    if let Some(top) = self.stack.pop() {
+                        self.stack.push(top * top * top);
+                        self.print_top();
+                    }
+                },
+                OneOpt::Sqrt => {
+                    if let Some(top) = self.stack.pop() {
+                        self.stack.push(top.sqrt());
+                        self.print_top();
+                    }
+                }
+            },
+            Opt::TwoOpt(to) => {
+                if self.stack.len() >= 2 {
+                    if let Some(n2) = self.stack.pop() {
+                        if let Some(n1) = self.stack.pop() {
+                            match to {
+                                TwoOpt::Plus => self.stack.push( n1 + n2),
+                                TwoOpt::Minus => self.stack.push( n1 - n2),
+                                TwoOpt::Times => self.stack.push( n1 * n2),
+                                TwoOpt::Divided => if n2 < 1e-10 {
+                                    self.stack.push( n1 * n2);
+                                }
+                                else{
+                                    self.stack.push(n1);
+                                    self.stack.push(n2);
+                                }
+                            }
+                            self.print_top()
+                        }
+                    }
+                }
+            },
+            Opt::UtilOpt(uo) => {
+                match uo {
+                    UtilOpt::Print => self.print_top(),
+                    UtilOpt::PrintAll => self.print_all(),
+                }
             }
-            _ => todo!("implémenter tt les opt"),
         }
     }
 }
 
 
 fn main() {
-
     let mut stack = Box::new(Vec::<f64>::new());
+    let mut rpnstack = RPNStack::new(&mut stack);
+
     println!("Welcome to your new RPN calculator written in Rust !");
-
-    stack.push(4.2);
-
-    dbg!(stack.last());
 
     loop {
         let mut user_line = String::new();
         print!("> ");
         io::stdout().flush().unwrap();
         io::stdin().read_line(&mut user_line).expect("Failed to read user input");
-        dbg!(&user_line);
 
         for token in user_line.trim().split_whitespace() {
             match Opt::from_str(token) {
-                Ok(opt) => match opt {
-                    Opt::Number(n) => println!("Got number {} ", n),
-                    Opt::OneOpt(to) => {dbg!(to) ; stack ;   () },
-                    Opt::TwoOpt(to) => {dbg!(to) ; () },
-                },
+                Ok(opt) => {
+                    rpnstack.handle_opt(&opt);
+                }
                 Err(_) => println!("can't parse as opt"),
             }
 
         }
-
-
-
     }
 
 }
