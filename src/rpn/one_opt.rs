de75e6cf1159481b77a767943084c81df36d8a99
jerespi <jeremie.spiesser@gmail.com>
2024-07-17 10:22:13 +0200

    added some tests

--- src/rpn/one_opt.rs
+++ src/rpn/one_opt.rs
@@ -1,6 +1,6 @@
 use std::str::FromStr;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum OneOpt {
     Squared,
     Cubed,
@@ -18,6 +18,45 @@ impl FromStr for OneOpt {
             "**3" => Ok(OneOpt::Cubed),
             "sqrt" => Ok(OneOpt::Sqrt),
             _ => Err(ParseOneOptErr),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use crate::rpn::one_opt::{OneOpt, ParseOneOptErr};
+
+    #[test]
+    fn test_from_str_squared_ok() {
+        let s1 = "**2";
+        assert_eq!(OneOpt::from_str(s1).unwrap(), OneOpt::Squared);
+        let s1 = "  **2  ";
+        assert_eq!(OneOpt::from_str(s1).unwrap(), OneOpt::Squared);
+    }
+
+    #[test]
+    fn test_from_str_cube_ok() {
+        let s1 = "**3";
+        assert_eq!(OneOpt::from_str(s1).unwrap(), OneOpt::Cubed);
+        let s1 = " **3   ";
+        assert_eq!(OneOpt::from_str(s1).unwrap(), OneOpt::Cubed);
+    }
+
+    #[test]
+    fn test_from_str_sqrt_ok() {
+        let s1 = "sqrt  ";
+        assert_eq!(OneOpt::from_str(s1).unwrap(), OneOpt::Sqrt);
+        let s1 = " sqrt  ";
+        assert_eq!(OneOpt::from_str(s1).unwrap(), OneOpt::Sqrt);
+    }
+
+    #[test]
+    fn test_from_str_fails() {
+        let v = vec!["a**2", "sqrat", "**2 **3", "sqrt(2)"];
+        for t in v {
+            assert_eq!(OneOpt::from_str(t).err().unwrap(), ParseOneOptErr);
         }
     }
 }
--- src/rpn/two_opt.rs
+++ src/rpn/two_opt.rs
@@ -1,6 +1,6 @@
 use std::str::FromStr;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum TwoOpt {
     Plus,
     Minus,
@@ -20,6 +20,53 @@ impl FromStr for TwoOpt {
             "*" => Ok(TwoOpt::Times),
             "/" => Ok(TwoOpt::Divided),
             _ => Err(ParseTwoOptErr),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use crate::rpn::two_opt::{ParseTwoOptErr, TwoOpt};
+
+    #[test]
+    fn test_from_str_plus_ok() {
+        let s1 = "+";
+        assert_eq!(TwoOpt::from_str(s1).unwrap(), TwoOpt::Plus);
+        let s1 = "  +  ";
+        assert_eq!(TwoOpt::from_str(s1).unwrap(), TwoOpt::Plus);
+    }
+
+    #[test]
+    fn test_from_str_minus_ok() {
+        let s1 = "-";
+        assert_eq!(TwoOpt::from_str(s1).unwrap(), TwoOpt::Minus);
+        let s1 = " -   ";
+        assert_eq!(TwoOpt::from_str(s1).unwrap(), TwoOpt::Minus);
+    }
+
+    #[test]
+    fn test_from_str_times_ok() {
+        let s1 = "*  ";
+        assert_eq!(TwoOpt::from_str(s1).unwrap(), TwoOpt::Times);
+        let s1 = " *  ";
+        assert_eq!(TwoOpt::from_str(s1).unwrap(), TwoOpt::Times);
+    }
+
+    #[test]
+    fn test_from_str_divided_ok() {
+        let s1 = "/  ";
+        assert_eq!(TwoOpt::from_str(s1).unwrap(), TwoOpt::Divided);
+        let s1 = " /  ";
+        assert_eq!(TwoOpt::from_str(s1).unwrap(), TwoOpt::Divided);
+    }
+
+    #[test]
+    fn test_from_str_fails() {
+        let v = vec!["1+1", "2-3", "a*b", "**", "qlkjsdfh", "2 / 4"];
+        for t in v {
+            assert_eq!(TwoOpt::from_str(t).err().unwrap(), ParseTwoOptErr);
         }
     }
 }
